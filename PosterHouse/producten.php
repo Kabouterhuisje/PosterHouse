195236b3f5283131486f278f344817858c42c0e0
ShaunVanBeurden <smkbeurd@avans.nl>
2017-03-07 21:06:18 +0100

    Het aantal producten wordt nu opgehaald uit de database en weergegeven

--- PosterHouse/producten.php
+++ PosterHouse/producten.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
-include_once 'dbconnect.php';
+$connect = mysqli_connect("localhost", "root", "", "posterhouse_database");
 
 if (isset($_SESSION['userSession'])) {
-    $query = $DBcon->query("SELECT * FROM user WHERE user_id=".$_SESSION['userSession']);
+    $query = $connect->query("SELECT * FROM user WHERE user_id=".$_SESSION['userSession']);
     $userRow=$query->fetch_array();
 }
-
-$DBcon->close();
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml">
@@ -30,7 +27,7 @@ require 'header.php';
 
 <!-- sidemenu -->
 <!-- style="margin-top:12%;text-align:left; -->
-<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
+<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1" style="margin-top:12%;text-align:left;">
 	<p>Filter op categorie</p>
       <ul class="nav navbar-nav">
       <div class="col-lg-3">
@@ -66,49 +63,33 @@ require 'header.php';
         </div>
       </ul>
     </div>
-</div>
 
 <!-- artikelen -->
 <div class="container">
 	<div class="row" style="margin-bottom:2%; text-align:center;">
 		<h2>Artikelen</h2>
 	</div>
-	<div class="row">
-		<div class="col-xs-6 col-md-3">
-			<img src="images/artikel.png"/>
-			<p>Placeholder</p>
-		</div>
-		<div class="col-xs-6 col-md-3">
-			<img src="images/artikel.png"/>
-			<p>Placeholder</p>
-		</div>
-		<div class="col-xs-6 col-md-3">
-			<img src="images/artikel.png"/>
-			<p>Placeholder</p>
-		</div>
-		<div class="col-xs-6 col-md-3">
-			<img src="images/artikel.png"/>
-			<p>Placeholder</p>
-		</div>
-	</div>	
-	<div class="row">
-		<div class="col-xs-6 col-md-3">
-			<img src="images/artikel.png"/>
-			<p>Placeholder</p>
-		</div>
-		<div class="col-xs-6 col-md-3">
-			<img src="images/artikel.png"/>
-			<p>Placeholder</p>
-		</div>
-		<div class="col-xs-6 col-md-3">
-			<img src="images/artikel.png"/>
-			<p>Placeholder</p>
-		</div>
-		<div class="col-xs-6 col-md-3">
-			<img src="images/artikel.png"/>
-			<p>Placeholder</p>
-		</div>
-	</div>	
+	<?php 
+	$query = "SELECT * FROM product";
+	$result = mysqli_query($connect, $query);
+	$num_rows = mysqli_num_rows($result);
+	
+	if($num_rows > 0)
+	{
+		while($row = mysqli_fetch_array($result)) 
+		{
+			for ($i = 0; $i < $num_rows; $i++)
+			{
+	?>
+	<div class="col-xs-6 col-md-3">
+		<img src="images/artikel.png"/>
+		<p>Placeholder</p>
+	</div>
+	<?php 
+			}
+		}
+	}
+	?>
 </div>
 
 <!-- Paginering -->
