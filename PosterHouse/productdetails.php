8d9f589ce30dbb8a4b7f5d511bd21092c6e6245a
ShaunVanBeurden <smkbeurd@avans.nl>
2017-03-16 00:28:31 +0100

    added product detail page

---
+++ PosterHouse/productdetails.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+$connect = mysqli_connect("localhost", "root", "", "posterhouse_databaseV3");
+
+if (isset($_SESSION['userSession'])) {
+	$query = $connect->query("SELECT * FROM user WHERE user_id=".$_SESSION['userSession']);
+	$userRow=$query->fetch_array();
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Welcome</title>
+        <!-- Verwijzingen -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+    </head>
+<body>
+
+<?php
+require 'header.php';
+?>
+
+<?php 
+		$query = "SELECT * FROM product where id = " . $_GET['id'];
+		$result = mysqli_query($connect, $query);
+		$row = mysqli_fetch_array($result);
+?>
+
+<div class="container-fluid">
+    <div class="content-wrapper">	
+		<div class="item-container">	
+			<div class="container">	
+				<div class="col-md-12">
+					<div class="img-responsive center-block" style="margin-top:8%;">
+					<center>
+						<img id="item-display" src="images/posters/<?php echo $row['image'];?>" height="400" width="250"></img>
+					</center>
+					</div>
+				</div>	
+				<div style="text-align:center;">
+					<h3><?php echo $row['product_name'];?></h3>
+					<p><?php echo $row['price'];?></p>
+					<p><?php echo $row['description'];?></p>
+					<div class="btn-group cart">
+						<form method="post" action="winkelmandje.php?action=add&id=<?php echo $row['id'];?>">
+							<input type="text" name="quantity" class="form-control" value="1" />
+	        				<input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />	
+        				</form>
+					</div>
+				</div>
+			</div> 
+		</div>
+	</div>
+</div>
+
+<?php
+require 'footer.php';
+$connect->close();
+?>
+
+</body>
+</html>
