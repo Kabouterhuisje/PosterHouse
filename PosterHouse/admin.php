2972dacfdd1020d1681a7704b2e473fcfe355686
Dennis Tijbosch <dtijbosch@student.avans.nl>
2017-03-17 17:56:15 +0100

    added tab layout for admin panel

--- PosterHouse/admin.php
+++ PosterHouse/admin.php
@@ -25,9 +25,39 @@ $connect->close();
 require 'header.php';
 ?>
 <div class="container"><br /><br /><br /><br />
-    <div class="jumbotron">
-        <h1>Coming Soon</h1>
-    </div>
+
+        <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+            <li><a data-toggle="tab" href="#menu1">Producten</a></li>
+            <li><a data-toggle="tab" href="#menu2">Categoriën</a></li>
+            <li><a data-toggle="tab" href="#menu3">Subcategoriën</a></li>
+            <li><a data-toggle="tab" href="#menu4">Bestellingen</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div id="home" class="tab-pane fade in active"><br />
+                <div class="jumbotron">
+                    <h1>Welkom, administrator!</h1>
+                    <p>U bevind zich op het admin panel. Beheer makkelijk de webshop door dingen toe te voegen, verwijderen of wijzigen. Klik op een tab om aan de slag te gaan!</p>
+                </div>
+            </div>
+            <div id="menu1" class="tab-pane fade">
+                <h3>Producten</h3>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div id="menu2" class="tab-pane fade">
+                <h3>Categoriën</h3>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+            </div>
+            <div id="menu3" class="tab-pane fade">
+                <h3>Subcategoriën</h3>
+                <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+            </div>
+            <div id="menu4" class="tab-pane fade">
+                <h3>Bestellingen</h3>
+                <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+            </div>
+        </div>
 </div>
 <?php
 require 'footer.php';
