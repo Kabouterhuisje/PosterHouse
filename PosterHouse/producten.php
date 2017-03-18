d1679892453daeb905d7ace6719549e85b6ca244
ShaunVanBeurden <smkbeurd@avans.nl>
2017-03-18 13:57:31 +0100

    Temporary content behind content fix

--- PosterHouse/producten.php
+++ PosterHouse/producten.php
@@ -20,7 +20,7 @@ if (isset($_SESSION['userSession'])) {
         <script src="bootstrap/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     </head>
-<body>
+<body style="padding-bottom: 167px;">
 
 <?php
 require 'header.php';
