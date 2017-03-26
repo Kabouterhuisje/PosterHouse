5cdf55bfa2b3a82f4a56545fc3d9a3e507bcf10d
Dennis Tijbosch <dtijbosch@student.avans.nl>
2017-03-26 15:07:48 +0200

    db update to V6

--- PosterHouse/dbconnect.php
+++ PosterHouse/dbconnect.php
@@ -1,11 +1,11 @@
 <?php
 
-    $connect = mysqli_connect("localhost", "root", "", "posterhouse_databaseV5");
+    $connect = mysqli_connect("localhost", "root", "", "posterhouse_databaseV6");
     
      if ($connect->connect_errno) {
          die("ERROR : -> ".$connect->connect_error);
      }
 
-     $DBconnect = new mysqli("localhost", "root", "", "posterhouse_databaseV5");
+     $DBconnect = new mysqli("localhost", "root", "", "posterhouse_databaseV6");
 
 ?>
