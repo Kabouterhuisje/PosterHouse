558abe4cdcd0339b60c64f4b9c7d6f4a9dc762cc
ShaunVanBeurden <smkbeurd@avans.nl>
2017-03-29 12:01:58 +0200

    Added commentary to the homepage

--- PosterHouse/index.php
+++ PosterHouse/index.php
@@ -1,7 +1,9 @@
 <?php
+// We starten de sessie en includen de database connectie
 session_start();
 include 'dbconnect.php';
 
+// Checkt of de usersessie is gezet
 if (isset($_SESSION['userSession'])) {
     $query = $connect->query("SELECT * FROM user WHERE user_id=".$_SESSION['userSession']);
     $userRow=$query->fetch_array();
@@ -65,9 +67,7 @@ require 'header.php';
 
 <?php
 require 'footer.php';
-
 ?>
 
 </body>
 </html>
-
