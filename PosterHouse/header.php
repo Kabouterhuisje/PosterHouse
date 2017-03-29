2e8279673b3d02e304f3caf9367b6a1e295523fa
ShaunVanBeurden <smkbeurd@avans.nl>
2017-03-29 12:12:37 +0200

    Added commentary to the header

--- PosterHouse/header.php
+++ PosterHouse/header.php
@@ -1,10 +1,12 @@
 <?php
-
+// We includen de database connectie
 include 'dbconnect.php';
 
+// Het menu wordt uit de database opgehaald
 $menuQuery = "SELECT menuitem_name, menuitem_link FROM menuitem ORDER BY id ASC";
 $result = $connect->query($menuQuery);
 
+// Stopt elk menuitem in een array
 while($row = $result->fetch_array()) {
     $rows[] = $row;
 }
@@ -25,11 +27,14 @@ while($row = $result->fetch_array()) {
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <?php
+                // We gaan elk menuitem af
                 foreach ($rows as $row) {
+                	// Zet de link in een variabele
                     $menuItemLink = $row['menuitem_link'];
                     // Checkt of de link van de huidige pagina overeenkomt met die uit de database
                     if ($menuItemLink == basename($_SERVER['PHP_SELF']))
                     {
+                    	// Zoja dan wordt hij active en kun je zien op welke pagina je zit
                     	echo "<li class='active'><a href='$menuItemLink'>".$row['menuitem_name']."</a></li>";
                     }
                     else
@@ -45,20 +50,26 @@ while($row = $result->fetch_array()) {
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                         <?php
+                        // Checkt of de usersessie is gezet
                         if (isset($_SESSION['userSession'])) {
                             echo $userRow['username'];
                         }
                         else {
                             echo 'Profiel';
                         }
-                        ?><span class="caret"></span></a>
+                        ?>
+                    <span class="caret"></span></a>
+                    <!-- Het dropdown menu van een gebruiker/gast -->
                     <ul class="dropdown-menu">
                         <?php
+                        // Als je usersessie niet is gezet kan je inloggen
                         if (!isset($_SESSION['userSession'])) {
                             echo '<li><a href="login.php">Inloggen</a></li>';
                         }
+                        // Als je usersessie wel is gezet kun je naar je profiel gaan en uitloggen
                         if (isset($_SESSION['userSession'])) {
                             echo '<li><a href="profile.php">Mijn Account</a></li>';
+                            // Als de rol van de gebruiker Admin is dan kan je naar het Admin panel gaan
                             if ($userRow['role'] == 'Admin') {
                                 echo '<li><a href="admin.php">Admin panel</a></li>';
                             }
@@ -70,6 +81,7 @@ while($row = $result->fetch_array()) {
             </ul>
             <div class="nav navbar-nav form-inline navbar-right" style="padding: 10px;">
                 <div class="input-group">
+                <!-- Zoekbalk -->
                 <form action="producten.php" method="get">
                     <input type="text" name="searchbar" class="form-control"></input>
                     <div class="input-group-btn">
@@ -83,5 +95,6 @@ while($row = $result->fetch_array()) {
 </nav>
 
 <?php
+// Sluit de databaseconnectie
 $connect->close();
 ?>
